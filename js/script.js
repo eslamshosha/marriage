b2f7dff50aaf3bb95b37b8f85287088a788c3750
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2022-12-31 21:52:44 +0200

    add

--- js/script.js
+++ js/script.js
@@ -9,35 +9,38 @@ $(document).ready(function () {
   new WOW().init();
 
   //otp code animation
-  $(".digit-group")
-    .find("input")
-    .each(function () {
-      $(this).attr("maxlength", 1);
-      $(this).on("keyup", function (e) {
-        var parent = $($(this).parent());
-
-        if (e.keyCode === 8 || e.keyCode === 37) {
-          var prev = parent.find("input#" + $(this).data("previous"));
-
-          if (prev.length) {
-            $(prev).select();
-          }
-        } else if (
-          (e.keyCode >= 48 && e.keyCode <= 57) ||
-          (e.keyCode >= 65 && e.keyCode <= 90) ||
-          (e.keyCode >= 96 && e.keyCode <= 105) ||
-          e.keyCode === 39
-        ) {
-          var next = parent.find("input#" + $(this).data("next"));
-
-          if (next.length) {
-            $(next).select();
-          } else {
-            if (parent.data("autosubmit")) {
-              parent.submit();
-            }
-          }
-        }
+  $(".otp-form *:input[type!=hidden]:first").focus();
+  let otp_fields = $(".otp-form .otp-field"),
+    otp_value_field = $(".otp-form .otp-value");
+  otp_fields
+    .on("input", function (e) {
+      $(this).val(
+        $(this)
+          .val()
+          .replace(/[^0-9]/g, "")
+      );
+      let opt_value = "";
+      otp_fields.each(function () {
+        let field_value = $(this).val();
+        if (field_value != "") opt_value += field_value;
+      });
+      otp_value_field.val(opt_value);
+    })
+    .on("keyup", function (e) {
+      let key = e.keyCode || e.charCode;
+      if (key == 8 || key == 46 || key == 37 || key == 40) {
+        // Backspace or Delete or Left Arrow or Down Arrow
+        $(this).prev().focus();
+      } else if (key == 38 || key == 39 || $(this).val() != "") {
+        // Right Arrow or Top Arrow or Value not empty
+        $(this).next().focus();
+      }
+    })
+    .on("paste", function (e) {
+      let paste_data = e.originalEvent.clipboardData.getData("text");
+      let paste_data_splitted = paste_data.split("");
+      $.each(paste_data_splitted, function (index, value) {
+        otp_fields.eq(index).val(value);
       });
     });
   //otp timer
@@ -67,15 +70,15 @@ $(document).ready(function () {
     $("#menu-id").click(function (e) {
       e.preventDefault();
       $(".page-sidebar").toggleClass("reset-left");
+      $(".page-content-wrapper").toggleClass("active");
       $("body").toggleClass("overflow");
-      $(".menu-bars .bars").toggleClass("open-bars");
-      $(".menu-bars .bars").toggleClass("close-bars");
+      $(".overlay-box").fadeToggle("500");
     });
-    $(".nav-head .close-btn").click(function () {
+    $(".sidebar-close, .overlay-box").click(function () {
       $(".page-sidebar").removeClass("reset-left");
-      $(".menu-bars .bars").toggleClass("open-bars");
-      $(".menu-bars .bars").toggleClass("close-bars");
+      $(".page-content-wrapper").toggleClass("active");
       $("body").removeClass("overflow");
+      $(".overlay-box").fadeToggle("500");
     });
   }
   //sidebar collpase
@@ -235,8 +238,7 @@ function showPass(showPass) {
   }
 }
 //file input
-$(".custom-file-upload .upload-change").change(function (e) 
-{
+$(".custom-file-upload .upload-change").change(function (e) {
   let file_val;
   if ($(this).val() == "") {
     file_val = $(".file-txt").data("title");
@@ -255,4 +257,167 @@ if (dateExists) {
     maxDate: new Date().fp_incr(14), // 14 days from now
     defaultDate: "today",
   });
+}
+
+// var months = ["يناير", "فبراير", "مارس", "إبريل", "مايو", "يونيو",
+//               "يوليو", "أغسطس", "سبتمبر", "أكتوبر", "نوفمبر", "ديسمبر"];
+
+// var days =["اﻷحد","اﻷثنين","الثلاثاء","اﻷربعاء","الخميس","الجمعة","السبت"];
+//calender
+const calenderExists = document.getElementsByClassName("calendar").length > 0;
+if (calenderExists) {
+  class Calendar {
+    constructor() {
+      this.monthDiv = document.querySelector(".cal-month__current");
+      this.headDivs = document.querySelectorAll(".cal-head__day");
+      this.bodyDivs = document.querySelectorAll(".cal-body__day");
+      this.nextDiv = document.querySelector(".cal-month__next");
+      this.prevDiv = document.querySelector(".cal-month__previous");
+    }
+
+    init() {
+      moment.locale(window.navigator.userLanguage || window.navigator.language);
+
+      this.month = moment();
+      this.today = this.selected = this.month.clone();
+      this.weekDays = moment.weekdaysShort(true);
+
+      this.headDivs.forEach((day, index) => {
+        day.innerText = this.weekDays[index];
+      });
+
+      this.nextDiv.addEventListener("click", (_) => {
+        this.addMonth();
+      });
+      this.prevDiv.addEventListener("click", (_) => {
+        this.removeMonth();
+      });
+
+      this.bodyDivs.forEach((day) => {
+        day.addEventListener("click", (e) => {
+          const date =
+            +e.target.innerHTML < 10
+              ? `0${e.target.innerHTML}`
+              : e.target.innerHTML;
+
+          if (e.target.classList.contains("cal-day__month--next")) {
+            this.selected = moment(
+              `${this.month.add(1, "month").format("YYYY-MM")}-${date}`
+            );
+          } else if (e.target.classList.contains("cal-day__month--previous")) {
+            this.selected = moment(
+              `${this.month.subtract(1, "month").format("YYYY-MM")}-${date}`
+            );
+          } else {
+            this.selected = moment(`${this.month.format("YYYY-MM")}-${date}`);
+          }
+
+          this.update();
+        });
+      });
+
+      this.update();
+    }
+
+    update() {
+      this.calendarDays = {
+        first: this.month.clone().startOf("month").startOf("week").date(),
+        last: this.month.clone().endOf("month").date(),
+      };
+
+      this.monthDays = {
+        lastPrevious: this.month
+          .clone()
+          .subtract(1, "months")
+          .endOf("month")
+          .date(),
+        lastCurrent: this.month.clone().endOf("month").date(),
+      };
+
+      this.monthString = this.month.clone().format("MMMM YYYY");
+
+      this.draw();
+    }
+
+    addMonth() {
+      this.month.add(1, "month");
+
+      this.update();
+    }
+
+    removeMonth() {
+      this.month.subtract(1, "month");
+
+      this.update();
+    }
+
+    draw() {
+      this.monthDiv.innerText = this.monthString;
+
+      let index = 0;
+
+      if (this.calendarDays.first > 1) {
+        for (
+          let day = this.calendarDays.first;
+          day <= this.monthDays.lastPrevious;
+          index++
+        ) {
+          this.bodyDivs[index].innerText = day++;
+
+          this.cleanCssClasses(false, index);
+
+          this.bodyDivs[index].classList.add("cal-day__month--previous");
+        }
+      }
+
+      let isNextMonth = false;
+      for (let day = 1; index <= this.bodyDivs.length - 1; index++) {
+        if (day > this.monthDays.lastCurrent) {
+          day = 1;
+          isNextMonth = true;
+        }
+
+        this.cleanCssClasses(true, index);
+
+        if (!isNextMonth) {
+          if (
+            day === this.today.date() &&
+            this.today.isSame(this.month, "day")
+          ) {
+            this.bodyDivs[index].classList.add("cal-day__day--today");
+          }
+
+          if (
+            day === this.selected.date() &&
+            this.selected.isSame(this.month, "month")
+          ) {
+            this.bodyDivs[index].classList.add("cal-day__day--selected");
+          }
+
+          this.bodyDivs[index].classList.add("cal-day__month--current");
+        } else {
+          this.bodyDivs[index].classList.add("cal-day__month--next");
+        }
+
+        this.bodyDivs[index].innerText = day++;
+      }
+    }
+
+    cleanCssClasses(selected, index) {
+      this.bodyDivs[index].classList.contains("cal-day__month--next") &&
+        this.bodyDivs[index].classList.remove("cal-day__month--next");
+      this.bodyDivs[index].classList.contains("cal-day__month--previous") &&
+        this.bodyDivs[index].classList.remove("cal-day__month--previous");
+      this.bodyDivs[index].classList.contains("cal-day__month--current") &&
+        this.bodyDivs[index].classList.remove("cal-day__month--current");
+      this.bodyDivs[index].classList.contains("cal-day__day--today") &&
+        this.bodyDivs[index].classList.remove("cal-day__day--today");
+      if (selected) {
+        this.bodyDivs[index].classList.contains("cal-day__day--selected") &&
+          this.bodyDivs[index].classList.remove("cal-day__day--selected");
+      }
+    }
+  }
+  const cal = new Calendar();
+  cal.init();
 }
